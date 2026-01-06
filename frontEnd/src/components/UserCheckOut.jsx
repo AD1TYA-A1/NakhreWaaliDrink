134068fff19cdc68bd73c7bc37d30ce8d651ac38
Aditya Gaur <154731001+AD1TYA-A1@users.noreply.github.com>
2026-01-06 15:05:05 +0530

    Update API endpoint for user checkout

--- frontEnd/src/components/UserCheckOut.jsx
+++ frontEnd/src/components/UserCheckOut.jsx
@@ -95,7 +95,7 @@ const UserCheckout = () => {
       redirect: "follow"
     };
 
-    fetch("https://nakhredrinkbackend.onrender.com/api/users/api/userCheckOut", requestOptions)
+    fetch("https://drinksback.onrender.com/api/users/api/userCheckOut", requestOptions)
       .then((response) => response.text())
       .then((result) => {
         toast('Order Placed Redirecting to orders Section', {
